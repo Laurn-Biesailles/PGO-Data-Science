4798206faa6d9d717df4ce4aa34254b571ea642e
AmirFarasati <90319028+AmirFarasati@users.noreply.github.com>
2026-03-09 20:01:51 -0700

    Update analysis.h

--- titanic/analysis.h
+++ titanic/analysis.h
@@ -2,11 +2,14 @@
 #define ANALYSIS_H
 
 #include "dataset.h"
+#include <vector>
 
 // Runs the full Titanic analysis by testing various condition combinations.
 class Analyzer {
     public:
         static void runAnalysis(const std::vector<Passenger>& passengers);
+    private:
+        static std::vector<Passenger> makeBootstrapSample(const std::vector<Passenger>& passengers);
 };
 
 #endif
